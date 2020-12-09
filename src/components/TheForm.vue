1655015fb2714f186899b8abf23ae145643a1663
Yari Andres <yari.andres@gmail.com>
2020-12-09 23:37:12 +0100

    working with v-model and modifiers

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -2,11 +2,22 @@
   <form @submit.prevent="submitForm">
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName" />
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model="userName"
+      />
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input
+        id="age"
+        name="age"
+        type="number"
+        v-model.number="userAge"
+        ref="ageInput"
+      />
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
@@ -53,20 +64,26 @@
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                userName: ''
-            }
+export default {
+    data() {
+        return {
+        userName: '',
+        userAge: null
+        };
     },
-    
-    methods: {    
-        submitForm () {
-            console.log('Username :', this.userName);
-            this.userName = '';
-        }
+
+  methods: {
+    submitForm() {
+      console.log('Username :', this.userName);
+      this.userName = '';
+      console.log('User Age :');
+      console.log( this.userAge + 5);
+      console.log( this.$refs.ageInput.value + 5);
+      console.log(31);
+      this.userAge = '';
     }
-}
+  }
+};
 </script>
 
 <style scoped>
